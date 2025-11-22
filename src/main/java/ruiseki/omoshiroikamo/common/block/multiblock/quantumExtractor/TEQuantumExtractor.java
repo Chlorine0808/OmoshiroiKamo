cdcea493def210998bfcaf0c777950608ec605b8
Ruisu <akitomoya2908@gmail.com>
2025-11-22 17:22:35 +0700

    Add SlotGroup

--- src/main/java/ruiseki/omoshiroikamo/common/block/multiblock/quantumExtractor/TEQuantumExtractor.java
+++ src/main/java/ruiseki/omoshiroikamo/common/block/multiblock/quantumExtractor/TEQuantumExtractor.java
@@ -51,9 +51,7 @@ public abstract class TEQuantumExtractor extends AbstractMBModifierTE implements
     protected EnumDye focusColor = EnumDye.WHITE;
     protected float focusBoostModifier = 1.0F;
 
-    @SideOnly(Side.CLIENT)
     private float beamProgress = 0.0F;
-    @SideOnly(Side.CLIENT)
     private long lastBeamUpdateTick = 0L;
 
     public TEQuantumExtractor() {
@@ -391,10 +389,12 @@ public abstract class TEQuantumExtractor extends AbstractMBModifierTE implements
     }
 
     @Override
-    public void openInventory() {}
+    public void openInventory() {
+    }
 
     @Override
-    public void closeInventory() {}
+    public void closeInventory() {
+    }
 
     @Override
     public boolean isItemValidForSlot(int index, ItemStack stack) {
