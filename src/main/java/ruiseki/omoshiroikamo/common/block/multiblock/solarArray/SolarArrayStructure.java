4bfa233db7dae8c0f05065db3c49eea49ac799bd
Ruisu <akitomoya2908@gmail.com>
2025-11-29 09:43:48 +0700

    Add SolarArray T5-6

--- src/main/java/ruiseki/omoshiroikamo/common/block/multiblock/solarArray/SolarArrayStructure.java
+++ src/main/java/ruiseki/omoshiroikamo/common/block/multiblock/solarArray/SolarArrayStructure.java
@@ -1,7 +1,6 @@
 package ruiseki.omoshiroikamo.common.block.multiblock.solarArray;
 
 import static com.gtnewhorizon.structurelib.structure.StructureUtility.ofBlock;
-import static com.gtnewhorizon.structurelib.structure.StructureUtility.ofBlockAnyMeta;
 import static com.gtnewhorizon.structurelib.structure.StructureUtility.ofChain;
 import static com.gtnewhorizon.structurelib.structure.StructureUtility.transpose;
 import static ruiseki.omoshiroikamo.plugin.structureLib.StructureLibUtils.ofBlockAdderWithPos;
@@ -107,13 +106,84 @@ public class SolarArrayStructure {
             "           ",
             "           "
         }};
+    public static final String STRUCTURE_TIER_5 = "tier5";
+    public static final String[][] SHAPE_TIER_5 = new String[][]{
+        {
+            "FFFFFFFFFFFFF",
+            "FGGGGGGGGGGGF",
+            "FGGGGGGGGGGGF",
+            "FGGGGGGGGGGGF",
+            "FGGGGGGGGGGGF",
+            "FGGGGGGGGGGGF",
+            "FGGGGGGGGGGGF",
+            "FGGGGGGGGGGGF",
+            "FGGGGGGGGGGGF",
+            "FGGGGGGGGGGGF",
+            "FGGGGGGGGGGGF",
+            "FGGGGGGGGGGGF",
+            "FFFFFFFFFFFFF"
+        },
+        {
+            "             ",
+            "             ",
+            "             ",
+            "             ",
+            "    AA AA    ",
+            "    A   A    ",
+            "      Q      ",
+            "    A   A    ",
+            "    AA AA    ",
+            "             ",
+            "             ",
+            "             ",
+            "             "
+        }};
+    public static final String STRUCTURE_TIER_6 = "tier6";
+    public static final String[][] SHAPE_TIER_6 = new String[][]{
+        {
+            "FFFFFFFFFFFFFFF",
+            "FGGGGGGGGGGGGGF",
+            "FGGGGGGGGGGGGGF",
+            "FGGGGGGGGGGGGGF",
+            "FGGGGGGGGGGGGGF",
+            "FGGGGGGGGGGGGGF",
+            "FGGGGGGGGGGGGGF",
+            "FGGGGGGGGGGGGGF",
+            "FGGGGGGGGGGGGGF",
+            "FGGGGGGGGGGGGGF",
+            "FGGGGGGGGGGGGGF",
+            "FGGGGGGGGGGGGGF",
+            "FGGGGGGGGGGGGGF",
+            "FGGGGGGGGGGGGGF",
+            "FFFFFFFFFFFFFFF"
+        },
+        {
+            "               ",
+            "               ",
+            "               ",
+            "               ",
+            "               ",
+            "     AA AA     ",
+            "     A   A     ",
+            "       Q       ",
+            "     A   A     ",
+            "     AA AA     ",
+            "               ",
+            "               ",
+            "               ",
+            "               ",
+            "               "
+        }};
     // spotless:on
 
-    public static final int[][] TIER_OFFSET = { { 2, 1, 2 }, { 3, 1, 3 }, { 4, 1, 4 }, { 5, 1, 5 } };
+    public static final int[][] TIER_OFFSET = { { 2, 1, 2 }, { 3, 1, 3 }, { 4, 1, 4 }, { 5, 1, 5 }, { 6, 1, 6 },
+        { 7, 1, 7 } };
     public static IStructureDefinition<TESolarArrayT1> STRUCTURE_DEFINITION_TIER_1;
     public static IStructureDefinition<TESolarArrayT2> STRUCTURE_DEFINITION_TIER_2;
     public static IStructureDefinition<TESolarArrayT3> STRUCTURE_DEFINITION_TIER_3;
     public static IStructureDefinition<TESolarArrayT4> STRUCTURE_DEFINITION_TIER_4;
+    public static IStructureDefinition<TESolarArrayT5> STRUCTURE_DEFINITION_TIER_5;
+    public static IStructureDefinition<TESolarArrayT6> STRUCTURE_DEFINITION_TIER_6;
 
     @SuppressWarnings("unchecked")
     public static void registerStructureInfo() {
@@ -121,10 +191,17 @@ public class SolarArrayStructure {
         StructureDefinition.Builder<TESolarArrayT2> builder2 = StructureDefinition.builder();
         StructureDefinition.Builder<TESolarArrayT3> builder3 = StructureDefinition.builder();
         StructureDefinition.Builder<TESolarArrayT4> builder4 = StructureDefinition.builder();
+        StructureDefinition.Builder<TESolarArrayT5> builder5 = StructureDefinition.builder();
+        StructureDefinition.Builder<TESolarArrayT6> builder6 = StructureDefinition.builder();
 
         builder1.addShape(STRUCTURE_TIER_1, transpose(SHAPE_TIER_1))
             .addElement('Q', ofBlock(ModBlocks.SOLAR_ARRAY.get(), 0))
-            .addElement('G', ofBlockAnyMeta(ModBlocks.SOLAR_CELL.get(), 0))
+            .addElement('G', ofBlock(ModBlocks.SOLAR_ARRAY.get(), 0))
+            // .addElement(
+            // 'G',
+            // ofChain(
+            // ofBlockAdderWithPos(TESolarArray::addToMachine, ModBlocks.SOLAR_CELL.get(), 0),
+            // ofBlockAnyMeta(ModBlocks.SOLAR_CELL.get())))
             .addElement(
                 'A',
                 ofChain(
@@ -140,7 +217,12 @@ public class SolarArrayStructure {
 
         builder2.addShape(STRUCTURE_TIER_2, transpose(SHAPE_TIER_2))
             .addElement('Q', ofBlock(ModBlocks.SOLAR_ARRAY.get(), 1))
-            .addElement('G', ofBlockAnyMeta(ModBlocks.SOLAR_CELL.get(), 0))
+            .addElement('G', ofBlock(ModBlocks.SOLAR_ARRAY.get(), 0))
+            // .addElement(
+            // 'G',
+            // ofChain(
+            // ofBlockAdderWithPos(TESolarArray::addToMachine, ModBlocks.SOLAR_CELL.get(), 0),
+            // ofBlockAnyMeta(ModBlocks.SOLAR_CELL.get())))
             .addElement(
                 'A',
                 ofChain(
@@ -156,7 +238,12 @@ public class SolarArrayStructure {
 
         builder3.addShape(STRUCTURE_TIER_3, transpose(SHAPE_TIER_3))
             .addElement('Q', ofBlock(ModBlocks.SOLAR_ARRAY.get(), 2))
-            .addElement('G', ofBlockAnyMeta(ModBlocks.SOLAR_CELL.get(), 0))
+            .addElement('G', ofBlock(ModBlocks.SOLAR_ARRAY.get(), 0))
+            // .addElement(
+            // 'G',
+            // ofChain(
+            // ofBlockAdderWithPos(TESolarArray::addToMachine, ModBlocks.SOLAR_CELL.get(), 0),
+            // ofBlockAnyMeta(ModBlocks.SOLAR_CELL.get())))
             .addElement(
                 'A',
                 ofChain(
@@ -172,7 +259,12 @@ public class SolarArrayStructure {
 
         builder4.addShape(STRUCTURE_TIER_4, transpose(SHAPE_TIER_4))
             .addElement('Q', ofBlock(ModBlocks.SOLAR_ARRAY.get(), 3))
-            .addElement('G', ofBlockAnyMeta(ModBlocks.SOLAR_CELL.get(), 0))
+            .addElement('G', ofBlock(ModBlocks.SOLAR_ARRAY.get(), 0))
+            // .addElement(
+            // 'G',
+            // ofChain(
+            // ofBlockAdderWithPos(TESolarArray::addToMachine, ModBlocks.SOLAR_CELL.get(), 0),
+            // ofBlockAnyMeta(ModBlocks.SOLAR_CELL.get())))
             .addElement(
                 'A',
                 ofChain(
@@ -186,6 +278,48 @@ public class SolarArrayStructure {
                     ofBlock(ModBlocks.HARDENED_STRUCTURE.get(), 3),
                     ofBlock(ModBlocks.ALABASTER_STRUCTURE.get(), 3)));
 
+        builder5.addShape(STRUCTURE_TIER_5, transpose(SHAPE_TIER_5))
+            .addElement('Q', ofBlock(ModBlocks.SOLAR_ARRAY.get(), 4))
+            .addElement('G', ofBlock(ModBlocks.SOLAR_ARRAY.get(), 0))
+            // .addElement(
+            // 'G',
+            // ofChain(
+            // ofBlockAdderWithPos(TESolarArray::addToMachine, ModBlocks.SOLAR_CELL.get(), 0),
+            // ofBlockAnyMeta(ModBlocks.SOLAR_CELL.get())))
+            .addElement(
+                'A',
+                ofChain(
+                    ofBlockAdderWithPos(TESolarArray::addToMachine, ModBlocks.MODIFIER_NULL.get(), 0),
+                    ofBlock(ModBlocks.MODIFIER_NULL.get(), 0),
+                    ofBlock(ModBlocks.MODIFIER_PIEZO.get(), 0)))
+            .addElement(
+                'F',
+                ofChain(
+                    ofBlock(ModBlocks.BASALT_STRUCTURE.get(), 4),
+                    ofBlock(ModBlocks.HARDENED_STRUCTURE.get(), 4),
+                    ofBlock(ModBlocks.ALABASTER_STRUCTURE.get(), 4)));
+
+        builder5.addShape(STRUCTURE_TIER_6, transpose(SHAPE_TIER_6))
+            .addElement('Q', ofBlock(ModBlocks.SOLAR_ARRAY.get(), 5))
+            .addElement('G', ofBlock(ModBlocks.SOLAR_ARRAY.get(), 0))
+            // .addElement(
+            // 'G',
+            // ofChain(
+            // ofBlockAdderWithPos(TESolarArray::addToMachine, ModBlocks.SOLAR_CELL.get(), 0),
+            // ofBlockAnyMeta(ModBlocks.SOLAR_CELL.get())))
+            .addElement(
+                'A',
+                ofChain(
+                    ofBlockAdderWithPos(TESolarArray::addToMachine, ModBlocks.MODIFIER_NULL.get(), 0),
+                    ofBlock(ModBlocks.MODIFIER_NULL.get(), 0),
+                    ofBlock(ModBlocks.MODIFIER_PIEZO.get(), 0)))
+            .addElement(
+                'F',
+                ofChain(
+                    ofBlock(ModBlocks.BASALT_STRUCTURE.get(), 5),
+                    ofBlock(ModBlocks.HARDENED_STRUCTURE.get(), 5),
+                    ofBlock(ModBlocks.ALABASTER_STRUCTURE.get(), 5)));
+
         IStructureDefinition<TESolarArrayT1> definition1 = builder1.build();
         STRUCTURE_DEFINITION_TIER_1 = definition1;
         IStructureDefinition<TESolarArrayT2> definition2 = builder2.build();
@@ -194,11 +328,17 @@ public class SolarArrayStructure {
         STRUCTURE_DEFINITION_TIER_3 = definition3;
         IStructureDefinition<TESolarArrayT4> definition4 = builder4.build();
         STRUCTURE_DEFINITION_TIER_4 = definition4;
+        IStructureDefinition<TESolarArrayT5> definition5 = builder5.build();
+        STRUCTURE_DEFINITION_TIER_5 = definition5;
+        IStructureDefinition<TESolarArrayT6> definition6 = builder6.build();
+        STRUCTURE_DEFINITION_TIER_6 = definition6;
 
         IMultiblockInfoContainer.registerTileClass(TESolarArrayT1.class, new MultiblockInfoContainerT1(definition1));
         IMultiblockInfoContainer.registerTileClass(TESolarArrayT2.class, new MultiblockInfoContainerT2(definition2));
         IMultiblockInfoContainer.registerTileClass(TESolarArrayT3.class, new MultiblockInfoContainerT3(definition3));
         IMultiblockInfoContainer.registerTileClass(TESolarArrayT4.class, new MultiblockInfoContainerT4(definition4));
+        IMultiblockInfoContainer.registerTileClass(TESolarArrayT5.class, new MultiblockInfoContainerT5(definition5));
+        IMultiblockInfoContainer.registerTileClass(TESolarArrayT6.class, new MultiblockInfoContainerT6(definition6));
 
         Logger.info("Register Solar Array Structure Info");
     }
@@ -407,6 +547,120 @@ public class SolarArrayStructure {
         @Override
         public int survivalConstruct(ItemStack triggerStack, int elementBudget, ISurvivalBuildEnvironment env,
             TESolarArrayT4 ctx, ExtendedFacing aSide) {
+            int built = 0;
+            int tier = ctx.getTier();
+            built = this.structure.survivalBuild(
+                ctx,
+                triggerStack,
+                ctx.getStructurePieceName(),
+                ctx.getWorldObj(),
+                ExtendedFacing.DEFAULT,
+                ctx.xCoord,
+                ctx.yCoord,
+                ctx.zCoord,
+                TIER_OFFSET[tier - 1][0],
+                TIER_OFFSET[tier - 1][1],
+                TIER_OFFSET[tier - 1][2],
+                elementBudget,
+                env,
+                false);
+
+            return built;
+        }
+
+        @Override
+        public String[] getDescription(ItemStack stackSize) {
+            return new String[0];
+        }
+    }
+
+    private static class MultiblockInfoContainerT5 implements IMultiblockInfoContainer<TESolarArrayT5> {
+
+        private final IStructureDefinition<TESolarArrayT5> structure;
+
+        public MultiblockInfoContainerT5(IStructureDefinition<TESolarArrayT5> structure) {
+            this.structure = structure;
+        }
+
+        @Override
+        public void construct(ItemStack triggerStack, boolean hintsOnly, TESolarArrayT5 ctx, ExtendedFacing aSide) {
+            int tier = ctx.getTier();
+
+            this.structure.buildOrHints(
+                ctx,
+                triggerStack,
+                ctx.getStructurePieceName(),
+                ctx.getWorldObj(),
+                ExtendedFacing.DEFAULT,
+                ctx.xCoord,
+                ctx.yCoord,
+                ctx.zCoord,
+                TIER_OFFSET[tier - 1][0],
+                TIER_OFFSET[tier - 1][1],
+                TIER_OFFSET[tier - 1][2],
+                hintsOnly);
+        }
+
+        @Override
+        public int survivalConstruct(ItemStack triggerStack, int elementBudget, ISurvivalBuildEnvironment env,
+            TESolarArrayT5 ctx, ExtendedFacing aSide) {
+            int built = 0;
+            int tier = ctx.getTier();
+            built = this.structure.survivalBuild(
+                ctx,
+                triggerStack,
+                ctx.getStructurePieceName(),
+                ctx.getWorldObj(),
+                ExtendedFacing.DEFAULT,
+                ctx.xCoord,
+                ctx.yCoord,
+                ctx.zCoord,
+                TIER_OFFSET[tier - 1][0],
+                TIER_OFFSET[tier - 1][1],
+                TIER_OFFSET[tier - 1][2],
+                elementBudget,
+                env,
+                false);
+
+            return built;
+        }
+
+        @Override
+        public String[] getDescription(ItemStack stackSize) {
+            return new String[0];
+        }
+    }
+
+    private static class MultiblockInfoContainerT6 implements IMultiblockInfoContainer<TESolarArrayT6> {
+
+        private final IStructureDefinition<TESolarArrayT6> structure;
+
+        public MultiblockInfoContainerT6(IStructureDefinition<TESolarArrayT6> structure) {
+            this.structure = structure;
+        }
+
+        @Override
+        public void construct(ItemStack triggerStack, boolean hintsOnly, TESolarArrayT6 ctx, ExtendedFacing aSide) {
+            int tier = ctx.getTier();
+
+            this.structure.buildOrHints(
+                ctx,
+                triggerStack,
+                ctx.getStructurePieceName(),
+                ctx.getWorldObj(),
+                ExtendedFacing.DEFAULT,
+                ctx.xCoord,
+                ctx.yCoord,
+                ctx.zCoord,
+                TIER_OFFSET[tier - 1][0],
+                TIER_OFFSET[tier - 1][1],
+                TIER_OFFSET[tier - 1][2],
+                hintsOnly);
+        }
+
+        @Override
+        public int survivalConstruct(ItemStack triggerStack, int elementBudget, ISurvivalBuildEnvironment env,
+            TESolarArrayT6 ctx, ExtendedFacing aSide) {
             int built = 0;
             int tier = ctx.getTier();
             built = this.structure.survivalBuild(
