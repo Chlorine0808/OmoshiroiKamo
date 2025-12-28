87c8dd8148cc515bf8b2cc2733d01ae0e9d16274
Ruisu <akitomoya2908@gmail.com>
2025-12-28 19:12:08 +0700

    spotless

--- src/main/java/ruiseki/omoshiroikamo/module/machinery/common/tile/energy/output/TEEnergyOutputPortT6.java
+++ src/main/java/ruiseki/omoshiroikamo/module/machinery/common/tile/energy/output/TEEnergyOutputPortT6.java
@@ -1,7 +1,5 @@
 package ruiseki.omoshiroikamo.module.machinery.common.tile.energy.output;
 
-import ruiseki.omoshiroikamo.module.machinery.common.tile.energy.input.TEEnergyInputPort;
-
 /**
  * Energy Input Port TileEntity.
  * Accepts RF energy for machine processing.
